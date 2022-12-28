a8a6cecabfbd64d040116ce797af7688f4b66401
Natalia Dushkina <natalia.dushkina@groupcdg.com>
2022-12-28 16:55:18 +0000

    refactor points: use numpy functions

--- points.py
+++ points.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
-
-def dot_product(a, b):
-    return sum([a[i] * b[i] for i in range(len(a))])
-
-def vectors_subtraction(a, b):
-    return [a[i] - b[i] for i in range(len(a))]    
+import numpy as np
 
 # https://math.stackexchange.com/questions/1472049/check-if-a-point-is-inside-a-rectangular-shaped-area-3d
 def point_inside_parallelepipede(x, p1, p2, p4, p5):
-    u = vectors_subtraction(p1, p2)
-    v = vectors_subtraction(p1, p4)
-    w = vectors_subtraction(p1, p5)
+    u = np.subtract(p1, p2)
+    v = np.subtract(p1, p4)
+    w = np.subtract(p1, p5)
 
-    if dot_product(u, x) >= dot_product(u, p2) and dot_product(u, x) <= dot_product(u, p1) \
-    and dot_product(v, x) >= dot_product(v, p4) and dot_product(v, x) <= dot_product(v, p1) \
-    and dot_product(w, x) >= dot_product(w, p5) and dot_product(w, x) <= dot_product(w, p1):
+    if np.dot(u, x) >= np.dot(u, p2) and np.dot(u, x) <= np.dot(u, p1) \
+    and np.dot(v, x) >= np.dot(v, p4) and np.dot(v, x) <= np.dot(v, p1) \
+    and np.dot(w, x) >= np.dot(w, p5) and np.dot(w, x) <= np.dot(w, p1):
         return True
     else:
         return False
--- points.test.py
+++ points.test.py
@@ -5,10 +5,6 @@ import unittest
 import points
 
 class TestPoints(unittest.TestCase):
-    def test_dot_product():
-        a = [1, 2, 3]
-        b = [4, 5, 6]
-        assert points.dot_product(a, b) == 32, 'should be 32'
 
     def test_point_inside_parallelepipede():
         # p1, p2, p4, p5
@@ -16,5 +12,4 @@ class TestPoints(unittest.TestCase):
         assert points.point_inside_parallelepipede([2,2,2], [0,0,0], [0,0,1], [0,1,0], [0,0,1]) == False, 'should be False' 
 
 if __name__ == "__main__":
-    TestPoints.test_dot_product()
     TestPoints.test_point_inside_parallelepipede()
